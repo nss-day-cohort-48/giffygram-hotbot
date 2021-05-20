0c4d462513f473b3bc39022be7d7a3f5087a56e4
Dakota Lambert <dakotalambertbiz@gmail.com>
2021-05-20 11:15:23 -0400

    Rendered login page
    Co-authored-by: Gabe Gonzales <gq.gonzales1@gmail.com>
    Co-authored-by: Ryan Sawyer <rsawyer7777@outlook.com>

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -1,12 +1,26 @@
-const apiURL = "http://localhost:8088"
+const apiURL = "http://localhost:3000"
 const applicationElement = document.querySelector(".giffygram")
 
 
 const applicationState = {
+    users: [],
     currentUser: {},
     feed: {
         chosenUser: null,
         displayFavorites: false,
         displayMessages: false
     }
+}
+
+export const fetchUsers = () => {
+
+    return fetch(`${apiURL}/users`)
+    .then((res) => res.json())
+    .then((usersResponse) => {
+        applicationState.users = usersResponse
+    })
+}
+
+export const getUsers = () => {
+    return [...applicationState.users]
 }
